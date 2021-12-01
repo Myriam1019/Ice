da6136eafc9456509220bf65a5ccb6560fa9ecd0
15718 <15718@LAPTOP-SS0766RE>
2021-12-01 16:30:24 -0500

    Cold weather & hot chocolate...

---
+++ src/Icy/ReallyIcy.java
@@ -0,0 +1,12 @@
+package Icy;
+
+public class ReallyIcy {
+	
+	public static void main(String[] args) {
+		System.out.println("Very icy...");
+		
+		System.out.println("Cold weather...");
+		System.out.println(" Hot chocolate...");
+	}
+
+}
